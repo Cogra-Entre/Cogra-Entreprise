6fc5888f42f090f364ca273e8c2db0dce0f21b29
Ian Kamau <kian99564@gmail.com>
2023-11-27 23:52:16 +0300

    Styling about page

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./../dist/output.css"
+import "./../dist/output.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Navbar from "./components/navbar/navbar.jsx";
